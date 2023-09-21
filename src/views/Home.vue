eedd27566f9a5eef0dd0ce8d56614d7c519d1e1e
Your NameAngelique-D <angelique.didillon@gmail.com>
2023-09-21 17:11:56 +0200

    ✅  color portfolio modified

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,18 +6,18 @@ const setIsOpen = () => {
    return isOpen.value = !isOpen.value;
 }
 const quality = ref([
-    'québecoise',
-    'curieuse',
-    'rigoureuse',
-    'persévérante',
-    'travaillante'
+    'Québecoise',
+    'Curieuse',
+    'Rigoureuse',
+    'Persévérante',
+    'Travaillante'
 ])
 
 </script>
 
 <template>
     <div class="main-container">
-        <p>Vous recherchez une développeuse {{ quality.value }} ?</p>
+        
         <div class="container-wrapper">
             <p @click="setIsOpen" class="mr-12 flex-end">
                 <span class="point">.</span>
@@ -29,8 +29,12 @@ const quality = ref([
                     Recherche activement un stage pour la période du 27 novembre au 22 décembre
                 </p>
             </div>
+            
         </div>
-        <div class="flower-banner"></div>
+        <div class="questions">
+            <p>Vous recherchez une développeuse <span class="quality">{{ quality[0] }}</span> ?</p> 
+        </div>
+        <!-- <div class="flower-banner"></div> -->
     </div>
 </template>
 
@@ -39,68 +43,73 @@ const quality = ref([
 <style scoped lang="scss">
 @import "../styles/settings.scss";
 
-
-// .loader {
-//   background-color: var(--main-color);
-//   overflow: hidden;
-//   width: 100%;
-//   height: 100%;
-//   position: fixed;
-//   top: 0; left: 0;
-//   display: flex;
-//   align-items: center;
-//   align-content: center; 
-//   justify-content: center;  
-// }
-
-
 .main-container {
-    display: flex;
-    justify-content: flex-end;
+    display: block;
 
-    .point {
-        font-size: 40px;
-        color: $secondary;
-        cursor: pointer;
+    .container-wrapper {
         display: flex;
-        align-items: center;
-        align-content: center; 
-        justify-content: center;  
+        align-items: flex-end;
+        flex-direction: column;
+        height: 200px;
 
-        & {
-            border-radius: 100%;
-            border: var(--size) solid var(-&-color);
-            margin: 1.5px;
-        }
+        .point {
+            font-size: 40px;
+            color: $secondary;
+            cursor: pointer;
+            display: flex;
+            align-items: center;
+            align-content: center; 
+            justify-content: center;  
 
-        &:nth-child(1) {
-            animation: preloader .6s ease-in-out alternate infinite;
+            & {
+                border-radius: 100%;
+                border: var(--size) solid var(-&-color);
+                margin: 1.5px;
+            }
 
-        }
-        &:nth-child(2) {
-            animation: preloader .6s ease-in-out alternate .2s infinite;
-        }
+            &:nth-child(1) {
+                animation: preloader .6s ease-in-out alternate infinite;
 
-        &:nth-child(3) {
-            animation: preloader .6s ease-in-out alternate .4s infinite;
-        }
+            }
+            &:nth-child(2) {
+                animation: preloader .6s ease-in-out alternate .2s infinite;
+            }
 
-        @keyframes preloader {
-            100% { 
-                transform: scale(2); 
-                color: $point-color;
+            &:nth-child(3) {
+                animation: preloader .6s ease-in-out alternate .4s infinite;
+            }
+
+            @keyframes preloader {
+                100% { 
+                    transform: scale(2); 
+                    color: $point-color;
+                }
             }
         }
+        .description {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 150px;
+            width: 500px;
+            border-bottom-left-radius: 50px;
+            background-color: $tertiary;
+        }
     }
-    .description {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        height: 150px;
-        width: 500px;
-        border-bottom-left-radius: 50px;
-        background-color: $tertiary;
+
+    .questions {
+        font-family: 'Allura', handwriting;
+        font-size: 40px;
+        color: $secondary;
+        margin-left: 60px;
+
+        .quality {
+            font-size: 3em; 
+            text-shadow: .03em .03em 0 $tertiary;
+        }
     }
+    
+    
 
     .flower-banner {
         position: absolute;
