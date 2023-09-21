8746d1ead3981801bf80a6ba0b77ac79eb18cbba
Your NameAngelique-D <angelique.didillon@gmail.com>
2023-09-21 14:38:07 +0200

    ✨ Add animated point

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -26,9 +26,6 @@ const quality = ref([
             </p>
             <div class="description pa-4 mr-12" v-show="isOpen">
                 <p>
-                    Développeuse Web et Web Mobile
-                </p>
-                <p>
                     Recherche activement un stage pour la période du 27 novembre au 22 décembre
                 </p>
             </div>
@@ -41,13 +38,59 @@ const quality = ref([
 
 <style scoped lang="scss">
 @import "../styles/settings.scss";
+
+
+// .loader {
+//   background-color: var(--main-color);
+//   overflow: hidden;
+//   width: 100%;
+//   height: 100%;
+//   position: fixed;
+//   top: 0; left: 0;
+//   display: flex;
+//   align-items: center;
+//   align-content: center; 
+//   justify-content: center;  
+// }
+
+
 .main-container {
     display: flex;
     justify-content: flex-end;
+
     .point {
-        font-size: 30px;
+        font-size: 40px;
         color: $secondary;
         cursor: pointer;
+        display: flex;
+        align-items: center;
+        align-content: center; 
+        justify-content: center;  
+
+        & {
+            border-radius: 100%;
+            border: var(--size) solid var(-&-color);
+            margin: 1.5px;
+        }
+
+        &:nth-child(1) {
+            animation: preloader .6s ease-in-out alternate infinite;
+
+        }
+        &:nth-child(2) {
+            animation: preloader .6s ease-in-out alternate .2s infinite;
+        }
+
+        &:nth-child(3) {
+            animation: preloader .6s ease-in-out alternate .4s infinite;
+        }
+
+        @keyframes preloader {
+            100% { 
+                transform: scale(2); 
+                color: $point-color;
+            }
+        }
     }
     .description {
         display: flex;
