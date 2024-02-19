2a2a0a7724fa3d8e92ee0b8c11204bb895ea9b6e
Angelique-D <angelique.didillon@gmail.com>
2024-02-19 15:58:38 +0100

    ✨ Add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-footer class="justify-center">
+    2024 - Portfolio de Angélique Didillon
+  </v-footer>
+</template>
+
+<style scoped lang="scss">
+@import "../styles/settings.scss";
+.v-footer {
+  background-color: $primary;
+  color: white;
+}
+</style>
