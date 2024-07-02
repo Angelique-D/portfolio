f342e96aa8e4e24bad88ddbd9abfe35e67d3c9b9
Angelique-D <angelique.didillon@gmail.com>
2024-07-02 09:45:53 +0200

    ♻️ Reinstall node modules

--- src/views/HangmanGame.vue
+++ src/views/HangmanGame.vue
@@ -5,7 +5,8 @@ import words from "@/components/hangmanGame/listOfWords";
 import NavBarGame from "@/components/NavBarGame.vue";
 
 const alphabet = ref(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
-   'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y']);
+   'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']);
+const words = ref(['pomme', 'banane', 'poire', 'ananas', 'orange','melon', 'fraise', 'bleuet', 'bébé']);
 const countTry = ref(5);
 const isCharFound = ref(false);
 const nbCharFound = ref(0);
